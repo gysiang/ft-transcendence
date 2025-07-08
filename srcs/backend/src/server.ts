eeaf0a53ff54db680ad0fdec93d2c2df609e7dba
gysiang <gysiang@gmail.com>
2025-07-08 16:55:11 +0800

    docker backend

--- srcs/backend/src/server.ts
+++ srcs/backend/src/server.ts
@@ -8,7 +8,7 @@ fastify.get('/api/hello', async () => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+   await fastify.listen({ port: 3000, host: '0.0.0.0' });
     console.log('Server running at http://localhost:3000')
   } catch (err) {
     fastify.log.error(err)
