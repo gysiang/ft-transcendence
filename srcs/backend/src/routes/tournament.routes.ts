0d21fc34f0a5cbb072c2966320d1c2f524e2bd7c
gysiang <gysiang@gmail.com>
2025-08-07 14:39:59 +0800

    post route to create new match

--- srcs/backend/src/routes/tournament.routes.ts
+++ srcs/backend/src/routes/tournament.routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { newTournament, getTournament } from '../controllers/tournament.controller';
+import { newMatch, getMatch } from '../controllers/match.controller';
 import { ITournamentParams } from '../models/tournament.model'
+import { IMatchParams } from '../models/match.model';
 
 export async function gameRoutes(app: FastifyInstance) {
 	app.post('/api/game', {preHandler: [app.authenticate]}, newTournament);
 	app.get<{Params: ITournamentParams}>('/api/game/:id',{preHandler: [app.authenticate]}, getTournament);
+	app.post('/api/game/match', {preHandler: [app.authenticate]}, newMatch);
+	app.get<{Params: IMatchParams}>('/api/game/match/:id',{preHandler: [app.authenticate]}, getMatch);
 
 }
